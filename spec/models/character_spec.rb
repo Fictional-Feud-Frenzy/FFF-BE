71fa427cf42532c25a43f0e76212ef9f4822a167
Nick Sacco <nicksacco10@gmail.com>
2023-10-12 16:04:12 -0700

    test: added allow nil validation tests
    CO-AUTHORED-BY <bmilton2412@gmail.com>

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -10,5 +10,16 @@ RSpec.describe Character, type: :model do
     it { should validate_presence_of :power }
     it { should validate_presence_of :combat }
     it { should validate_presence_of :image }
+    it { should allow_value(nil).for(:full_name) }
+    it { should allow_value(nil).for(:place_of_birth) }
+    it { should allow_value(nil).for(:publisher) }
+    it { should allow_value(nil).for(:alignment) }
+    it { should allow_value(nil).for(:gender) }
+    it { should allow_value(nil).for(:race) }
+    it { should allow_value(nil).for(:height) }
+    it { should allow_value(nil).for(:weight) }
+    it { should allow_value(nil).for(:eye_color) }
+    it { should allow_value(nil).for(:hair_color) }
+    it { should allow_value(nil).for(:group_affiliation) }
   end
 end
