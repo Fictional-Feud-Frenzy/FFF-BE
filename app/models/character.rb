49e664460d5863b97876c88a0aa07942e1b1eb7b
Nick Sacco <nicksacco10@gmail.com>
2023-10-12 15:51:51 -0700

    feat: added validation statements to model
    CO-AUTHORED-BY <bmilton2412@gmail.com>

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,10 @@
 class Character < ApplicationRecord
   validates :name, presence: true
+  validates :intelligence, presence: true
+  validates :strength, presence: true
+  validates :speed, presence: true
+  validates :durability, presence: true
+  validates :power, presence: true
+  validates :combat, presence: true
+  validates :image, presence: true
 end
