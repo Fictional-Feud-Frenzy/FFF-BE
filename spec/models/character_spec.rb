923e0bbc60ce6ad7057aa4e368d2978d7ae81ffe
Nick Sacco <nicksacco10@gmail.com>
2023-10-12 15:43:01 -0700

    test: character validation tests
    CO-AUTHORED-BY <bmilton2412@gmail.com

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Character, type: :model do
   describe 'validations' do
-    it { should validate_presence_of(:name) }
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :intelligence }
+    it { should validate_presence_of :strength }
+    it { should validate_presence_of :speed }
+    it { should validate_presence_of :durability }
+    it { should validate_presence_of :power }
+    it { should validate_presence_of :combat }
+    it { should validate_presence_of :image }
   end
 end
