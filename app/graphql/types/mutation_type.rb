b40291e8d8d3bae1ece9eac616571471c723e159
Bradley Milton <bmilton2412@gmail.com>
2023-10-16 10:55:17 -0700

    comment out all mutation things

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-module Types
-  class MutationType < Types::BaseObject
-    # field :create_character, mutation: Mutations::CreateCharacter
-  end
-end
+# module Types
+#   class MutationType < Types::BaseObject
+#     field :create_character, mutation: Mutations::CreateCharacter
+#   end
+# end
