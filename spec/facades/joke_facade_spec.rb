4c1109b88d79f0c535fbdd9276a05281d833016a
Nick Sacco <nicksacco10@gmail.com>
2023-10-16 13:35:35 -0700

    test: joke facade test complete

--- spec/facades/joke_facade_spec.rb
+++ spec/facades/joke_facade_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe JokeFacade do
   describe '#joke' do
     it 'returns a random Chuck Norris joke as a string', :vcr do
       joke = JokeFacade.joke
-
+      
       expect(joke).to be_a(String)
     end
   end
