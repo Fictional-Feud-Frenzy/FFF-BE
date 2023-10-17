c59961938e020f7644b5ed27bef096fa0334d637
Jeff Nelson <jpnelson85@gmail.com>
2023-10-17 17:45:42 -0500

    superhero service

---
+++ app/services/superhero_service.rb
@@ -0,0 +1,20 @@
+class SuperheroService
+
+  def self.get_superhero_data(id)
+    get_url("/api/10111528630893278/#{id}")
+  end
+
+  def get_url(url)
+    response = conn.get(url)
+    # puts "Raw Response: #{response.body}"
+    JSON.parse(response.body)
+  end
+
+  def conn
+    Faraday.new("https://superheroapi.com") do |faraday|
+    faraday.request :url_encoded
+    faraday.response :json, content_type: /\bjson$/
+    faraday.use FaradayMiddleware::FollowRedirects, limit: 5
+    end
+  end
+end
