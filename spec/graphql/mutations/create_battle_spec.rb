64b01a057c40360ed145d52c1c90f1c20f5c7162
Chris Cullinane <chriscullinane@protonmail.com>
2023-10-19 16:38:02 -0800

    add vcr cassette for createBattle mutation test

--- spec/graphql/mutations/create_battle_spec.rb
+++ spec/graphql/mutations/create_battle_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'CreateBattle', type: :mutation do
-  it 'creates a battle and returns the winner and description' do
+  it 'creates a battle and returns the winner and description', :vcr do
     character1 = Character.create(name: 'Character 1', intelligence: 80, strength: 90, speed: 70, durability: 85, power: 75, combat: 88, power_stats_weighted_average: 99, full_name: 'Full Name 1', publisher: 'Publisher 1', alignment: 'Good', image: 'image_url_1')
     character2 = Character.create(name: 'Character 2', intelligence: 85, strength: 92, speed: 75, durability: 80, power: 78, combat: 90, power_stats_weighted_average: 69, full_name: 'Full Name 2', publisher: 'Publisher 2', alignment: 'Evil', image: 'image_url_2')
     
