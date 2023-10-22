b1fe05340ba9855626f99fa6891ddbc2b1921c93
Ben Holden <bnhldn@gmail.com>
2023-10-22 13:46:26 -0600

    tweak the gpt request to be less than 300 tokens to be able to fit the frontend

--- app/services/chat_service.rb
+++ app/services/chat_service.rb
@@ -31,9 +31,9 @@ class ChatService
 
   def user_content
     if @mode == :norris
-      "#{@winner} ties #{@loser} in a fight, but then, out of nowhere, Chuck Norris descended from the heavens and defeated them both with a swift roundhouse kick. Please give a short funny story with no dialogue, panels, or markdown tags of how the battle went. The story should be less than 300 tokens."
+      "using less than 300 tokens provide a brief summer of the battle between #{@winner} winning against #{@loser} in an epic battle, but then, out of nowhere, Chuck Norris descended from the heavens and defeated them both with a swift roundhouse kick. Please give a short funny story with no dialogue, panels, or markdown tags of how the battle went. The story should be less than 300 tokens and make sure the story is concise and makes sense."
     else
-      "#{@winner} triumphed over #{@loser} in an epic battle. Please give a short story with no dialogue, panels, or markdown tags of how the battle went.  The story should be less than 300 tokens."
+      "using less than 300 tokens provide a brief summer of the battle between #{@winner} winning against #{@loser} in an epic battle. Please give a short story with no dialogue, panels, or markdown tags of how the battle went.  The story should be less than 300 tokens and make sure the story is concise and makes sense."
     end
   end
 
